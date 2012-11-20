f1455ca1b83032c0bb19874c63c6fa35cb68dd4b
Logic-Seeker <shobhabstha@gmail.com>
2012-11-20 22:30:58 +0545

    20Nov : Order form

--- src/com/brick/panel/OrderForm.java
+++ src/com/brick/panel/OrderForm.java
@@ -5,8 +5,6 @@ import java.awt.BorderLayout;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
-import javax.swing.JOptionPane;
-
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -14,14 +12,9 @@ import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
-
-import com.brick.database.DatabaseHelper;
 
 public class OrderForm extends JPanel {
 	private final JPanel panel = new JPanel();
@@ -39,33 +32,30 @@ public class OrderForm extends JPanel {
 	private final JTextField txtBrick = new JTextField();
 	private final JTextField txtHalf = new JTextField();
 	private final JButton btnOrder = new JButton("Order");
-	private JPanel panelOrderForm;
 
 	/**
 	 * Create the panel.
 	 */
 	public OrderForm() {
-		panelOrderForm=this;
 		txtHalf.setColumns(10);
 		txtBrick.setColumns(10);
 		txtDestination.setColumns(10);
 
 		initGUI();
 	}
-
 	private void initGUI() {
 		setLayout(new BorderLayout(0, 0));
-
+		
 		add(panel, BorderLayout.CENTER);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[] { 165, 200 };
-		gbl_panel.rowHeights = new int[] { 45, 45, 45, 45, 80 };
-		// gbl_panel.columnWidths = new int[]{0, 0, 0};
-		// gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_panel.columnWeights = new double[] { 0.0, 0.0 };
-		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0 };
+		gbl_panel.columnWidths = new int[]{165, 200};
+		gbl_panel.rowHeights = new int[]{45, 45, 45, 45, 80};
+		//gbl_panel.columnWidths = new int[]{0, 0, 0};
+		//gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0};
 		panel.setLayout(gbl_panel);
-
+		
 		GridBagConstraints gbc_lblCustomerName = new GridBagConstraints();
 		gbc_lblCustomerName.anchor = GridBagConstraints.WEST;
 		gbc_lblCustomerName.insets = new Insets(0, 0, 5, 5);
@@ -73,14 +63,14 @@ public class OrderForm extends JPanel {
 		gbc_lblCustomerName.gridy = 0;
 		lblCustomerName.setFont(new Font("Dialog", Font.BOLD, 14));
 		panel.add(lblCustomerName, gbc_lblCustomerName);
-
+		
 		GridBagConstraints gbc_comboBoxCustomerName = new GridBagConstraints();
 		gbc_comboBoxCustomerName.insets = new Insets(7, 0, 7, 0);
 		gbc_comboBoxCustomerName.fill = GridBagConstraints.BOTH;
 		gbc_comboBoxCustomerName.gridx = 1;
 		gbc_comboBoxCustomerName.gridy = 0;
 		panel.add(comboBoxCustomerName, gbc_comboBoxCustomerName);
-
+		
 		GridBagConstraints gbc_lblDestination = new GridBagConstraints();
 		gbc_lblDestination.anchor = GridBagConstraints.WEST;
 		gbc_lblDestination.insets = new Insets(0, 0, 5, 5);
@@ -88,14 +78,14 @@ public class OrderForm extends JPanel {
 		gbc_lblDestination.gridy = 1;
 		lblDestination.setFont(new Font("Dialog", Font.BOLD, 14));
 		panel.add(lblDestination, gbc_lblDestination);
-
+		
 		GridBagConstraints gbc_txtDestination = new GridBagConstraints();
 		gbc_txtDestination.insets = new Insets(7, 0, 7, 0);
 		gbc_txtDestination.fill = GridBagConstraints.BOTH;
 		gbc_txtDestination.gridx = 1;
 		gbc_txtDestination.gridy = 1;
 		panel.add(txtDestination, gbc_txtDestination);
-
+		
 		GridBagConstraints gbc_lblBrick = new GridBagConstraints();
 		gbc_lblBrick.anchor = GridBagConstraints.WEST;
 		gbc_lblBrick.insets = new Insets(0, 0, 5, 5);
@@ -103,14 +93,14 @@ public class OrderForm extends JPanel {
 		gbc_lblBrick.gridy = 2;
 		lblBrick.setFont(new Font("Dialog", Font.BOLD, 14));
 		panel.add(lblBrick, gbc_lblBrick);
-
+		
 		GridBagConstraints gbc_txtBrick = new GridBagConstraints();
 		gbc_txtBrick.insets = new Insets(7, 0, 7, 0);
 		gbc_txtBrick.fill = GridBagConstraints.BOTH;
 		gbc_txtBrick.gridx = 1;
 		gbc_txtBrick.gridy = 2;
 		panel.add(txtBrick, gbc_txtBrick);
-
+		
 		GridBagConstraints gbc_chckbxHalf = new GridBagConstraints();
 		gbc_chckbxHalf.anchor = GridBagConstraints.WEST;
 		gbc_chckbxHalf.insets = new Insets(0, 0, 5, 5);
@@ -118,14 +108,14 @@ public class OrderForm extends JPanel {
 		gbc_chckbxHalf.gridy = 3;
 		chckbxHalf.setFont(new Font("Dialog", Font.BOLD, 14));
 		panel.add(chckbxHalf, gbc_chckbxHalf);
-
+		
 		GridBagConstraints gbc_txtHalf = new GridBagConstraints();
 		gbc_txtHalf.insets = new Insets(7, 0, 7, 0);
 		gbc_txtHalf.fill = GridBagConstraints.BOTH;
 		gbc_txtHalf.gridx = 1;
 		gbc_txtHalf.gridy = 3;
 		panel.add(txtHalf, gbc_txtHalf);
-
+		
 		GridBagConstraints gbc_btnOrder = new GridBagConstraints();
 		gbc_btnOrder.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnOrder.gridwidth = 2;
@@ -135,37 +125,25 @@ public class OrderForm extends JPanel {
 		gbc_btnOrder.gridy = 4;
 		btnOrder.setFont(new Font("Dialog", Font.BOLD, 14));
 		panel.add(btnOrder, gbc_btnOrder);
-
+		
 		add(panel_1, BorderLayout.NORTH);
 		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
 		FlowLayout flowLayout = (FlowLayout) panel_2.getLayout();
 		flowLayout.setAlignment(FlowLayout.RIGHT);
 		panel_2.setBackground(Color.GRAY);
-
+		
 		panel_1.add(panel_2);
 		lblOrder.setFont(new Font("Dialog", Font.BOLD, 16));
 		lblOrder.setForeground(new Color(0, 191, 255));
-
+		
 		panel_2.add(lblOrder);
 		FlowLayout flowLayout_1 = (FlowLayout) panel_3.getLayout();
 		flowLayout_1.setAlignment(FlowLayout.RIGHT);
 		panel_3.setBackground(Color.GRAY);
-
+		
 		panel_1.add(panel_3);
-		button.setIcon(new ImageIcon("images/exit.png"));
-
+		button.setIcon(new ImageIcon("/home/shobhab/Development/workspace/Inventory Management1.0/images/exit.png"));
+		
 		panel_3.add(button);
-	}
-
-	public class CustomActionListner implements ActionListener {
-
-		public void actionPerformed(ActionEvent e) {
-			DatabaseHelper databaseHelper = new DatabaseHelper();
-			if (e.getSource() == btnOrder) {
-
-			} else if (e.getSource() == button) {
-				panelOrderForm.setVisible(false);
-			}
-		}
 	}
 }
